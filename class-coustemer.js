59d97760f6e4cbead3ae0fb6aa95449258f4cdd9
nesma manasarh <52491098+anaal@users.noreply.github.com>
2022-06-29 18:41:48 +0300

    add login page, asignup using html and checklogin using js

---
+++ class-coustemer.js
@@ -0,0 +1,40 @@
+class coustemer  {
+    name;
+    date;
+    salary;
+
+    constructor(nameCoustomer,date,salary){
+        this.setName(nameCoustomer);
+        this.setDate(date);
+        this.setSalary(salary);
+    }
+    setName(nameCoustomer){
+        if((typeof nameCoustomer === 'string') && (nameCoustomer.length > 2) && (firstName.length < 50 )){
+            this.nameCoustomer = nameCoustomer;
+        }
+    }
+    setDate(date){
+        this.setDate = date;
+    }
+    setSalary(salary){
+        if((typeof salary === 'number')){
+            this.salary = salary;
+        }
+      alert('the value insert is not a number')
+    }
+    getName(){
+        this.nameCoustomer ;
+    }
+    getDate(){  
+        this.date();
+    }
+    getCurrncy(){
+       this.currncy();
+    }
+
+    getallFunction(){
+        return `${this.getName()} ${this.getDate()} ${this.getCurrncy()}`;
+    }
+}
+
+let customer1 = new coustemer('cotode','1-2-2022','3000$');
