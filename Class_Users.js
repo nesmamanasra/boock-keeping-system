eaf445a227ee8ef253402b1cbdb07a0dd01934a8
BesanZeyad <107624772+BesanZeyad@users.noreply.github.com>
2022-06-29 16:55:43 +0300

    Creat user class , setter ang getter for user informaion and validation for email and business name

---
+++ Class_Users.js
@@ -0,0 +1,82 @@
+class Users{
+    email;
+    password;
+    buisness_name;
+    county;
+    currency;
+
+    constructor(email,password,buisness_name,county,currency){
+        this.setEmail(email);
+        this.setPassword(password);
+        this.setBuisnessName(buisness_name);
+        this.setCounty(county);
+        this.setCurrency(currency);
+    }
+    
+    ValidateEmail(inputText)
+    {
+       let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+       if(inputText.value.match(mailformat))
+         {    alert("Valid email address!");
+            }
+           else
+        {   alert("You have entered an invalid email address!");
+              return false;
+            }
+    }
+
+    ValidateName(name){
+        if(!(typeof(name)=='string')){
+            alert("You Should Enter String");
+        }
+    }
+
+    setEmail(email){
+        this.#ValidateEmail(email);
+        this.email=email;
+    }
+
+    getEmail(){
+        return this.email;
+    }
+
+    setPassword(password){
+        this.password=password;
+    }
+
+    getPassword(){
+        return this.password;
+    }
+
+    setBuisnessName(buisness_name){
+        ValidateName(buisness_name);
+        return this.buisness_name=buisness_name;
+    }
+
+    getBusinessName(){
+       
+        return this.buisness_name;
+    }
+
+    setCounty(county){
+        this.county=county;
+    }
+
+    getCounty(){
+        return this.county;
+    }
+    
+    setCurrency(currency){
+        this.currency=currency;
+    }
+
+    getCurrency(){
+        return this.currency;
+    }
+
+
+}
+
+let email=document.getElementById('email').value;
+let password =document.getElementById('password').value; 
+let buisness_name =document.getElementById('business_name').value;
