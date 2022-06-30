b6023912cb1e8ae17dbdfde3335915f3c4354c10
Nesma Manasra <52491098+nesmamanasra@users.noreply.github.com>
2022-06-30 17:04:21 +0300

    Update and rename Class_Users.js to class-uers.js
    -  Please change the file name as agreed to class-user.js
    - I made a review of the code so that there are many spaces between each code and code, and also in terms of function names and variables

--- class-uers.js
+++ class-uers.js
@@ -4,7 +4,6 @@ class Users{
     buisness_name;
     county;
     currency;
-
     constructor(email,password,buisness_name,county,currency){
         this.setEmail(email);
         this.setPassword(password);
@@ -17,64 +16,50 @@ class Users{
     {
        let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
        if(inputText.value.match(mailformat))
-         {    alert("Valid email address!");
+         { alert("Valid email address!");
             }
            else
-        {   alert("You have entered an invalid email address!");
-              return false;
+        { lert("You have entered an invalid email address!");
+             return false;
             }
     }
-
     ValidateName(name){
         if(!(typeof(name)=='string')){
             alert("You Should Enter String");
         }
     }
-
     setEmail(email){
         this.#ValidateEmail(email);
         this.email=email;
     }
-
     getEmail(){
         return this.email;
     }
-
     setPassword(password){
         this.password=password;
     }
-
     getPassword(){
         return this.password;
     }
-
     setBuisnessName(buisness_name){
         ValidateName(buisness_name);
         return this.buisness_name=buisness_name;
     }
-
     getBusinessName(){
-       
         return this.buisness_name;
     }
-
     setCounty(county){
         this.county=county;
     }
-
     getCounty(){
         return this.county;
-    }
-    
+    } 
     setCurrency(currency){
         this.currency=currency;
     }
-
     getCurrency(){
         return this.currency;
     }
-
-
 }
 
 let email=document.getElementById('email').value;
