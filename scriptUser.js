f5c9fbc41c6c25e5d3fc08b52a2fcf346f47a412
nesma manasarh <52491098+anaal@users.noreply.github.com>
2022-06-30 16:03:58 +0300

    update login page and signup page, store data in local storage

---
+++ scriptUser.js
@@ -0,0 +1,40 @@
+const signUp = e => {
+    let bname = document.getElementById('bname').value,
+        email = document.getElementById('email').value,
+        pwd = document.getElementById('pwd').value,
+        country = document.getElementById('country').value,
+        currency = document.getElementById('currency').value;
+
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+
+    let exist = formData.length && 
+        JSON.parse(localStorage.getItem('formData')).some(data => 
+            data.bname.toLowerCase() == bname.toLowerCase()
+        );
+
+    if(!exist){
+        formData.push({ bname, email, pwd, country, currency});
+        localStorage.setItem('formData', JSON.stringify(formData));
+        document.querySelector('form').reset();
+        document.getElementById('bname').focus();
+        alert("Account Created.\n\nPlease Sign In using the link below.");
+    }
+    else{
+        alert("Ooopppssss... Duplicate found!!!\nYou have already sigjned up");
+    }
+    e.preventDefault();
+}
+
+function signIn(e) {
+    let email = document.getElementById('email').value, pwd = document.getElementById('pwd').value;
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+    let exist = formData.length && 
+    JSON.parse(localStorage.getItem('formData')).some(data => data.email.toLowerCase() == email && data.pwd.toLowerCase() == pwd);
+    if(!exist){
+        alert("Incorrect login credentials");
+    }
+    else{
+        location.href = "main-page.html";
+    }
+    e.preventDefault();
+}
